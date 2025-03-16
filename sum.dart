644b6613d589bab594868bca37910048d2d75954
Aaron S Varghese <aaronsvarghese006@gmail.com>
2025-03-16 13:11:19 +0530

    Update sum.dart

--- sum.dart
+++ sum.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
 
 void main() {
-  print("Enter the first number:");
-  String? firstInput = stdin.readLineSync();
-  double? firstNumber = double.tryParse(firstInput ?? '0');
-  print("Enter the second number:");
-  String? secondInput = stdin.readLineSync();
-  double? secondNumber = double.tryParse(secondInput ?? '0');
-  double sum = (firstNumber ?? 0) + (secondNumber ?? 0);
-  String resultMessage = "The sum of $firstNumber and $secondNumber is $sum.";
-  print(resultMessage);
+  print('Welcome to the Sum Calculator\n');
+
+  print('Please enter the first number:');
+  double no1 = double.parse(stdin.readLineSync()!);
+
+  print('Please enter the second number:');
+  double no2 = double.parse(stdin.readLineSync()!);
+
+  double sum = no1 + no2;
+
+  print('\nThe sum of $no1 and $no2 is $sum');
 }
